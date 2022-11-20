59a80379ad2da085db1c604f6c238e620f72d379
movmovmovmov <kej708@gmail.com>
2022-11-20 16:18:05 +0900

    단위 테스트 코드 작성 및 확인 완료

---
+++ main/java/com/rloZI/exercise/Application.java
@@ -0,0 +1,14 @@
+package com.rloZI.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@SpringBootConfiguration
+public class Application {
+  public static void main(String[] args){
+      SpringApplication.run(Application.class, args);
+
+    }
+}
